2b540522d98ed8a8ab3e0c43f3ec74b77eecef27
Brigitte <114067290+brishar0n@users.noreply.github.com>
2024-12-15 19:32:36 +0700

    Update main.py

--- api/app/main.py
+++ api/app/main.py
@@ -7,17 +7,17 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 
 origins = [
-    "http://localhost:5173", 
+    "http://localhost:5173",
     "http://127.0.0.1:5173",
-    "https://air-quality-predictor-using-deep-learning.vercel.app/",
+    "https://air-quality-predictor-using-deep-learning.vercel.app",
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins, 
+    allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["*"],  
-    allow_headers=["*"], 
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 class PredictionRequest(BaseModel):
